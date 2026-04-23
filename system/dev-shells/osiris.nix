d712fab92a512ee0e3028393b0582ff8a23d50bf
tvistas <tiagovistas1@gmail.com>
2026-04-23 11:19:00 +0100

    Even smaller changes

--- system/dev-shells/osiris.nix
+++ system/dev-shells/osiris.nix
@@ -1,7 +1,11 @@
-{ pkgs }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   name = "osiris-shell";
+
+  venvDir = "./.venv";
 
   packages = with pkgs; [
     gcc
@@ -9,13 +13,32 @@ pkgs.mkShell {
     gnumake
     openmpi
     (hdf5.override {
-      enableFortran = true;
+      fortranSupport = true;
       mpiSupport = true;
+      cppSupport = false;
     })
     pkg-config
+
+    python3
+    python3Packages.venvShellHook
   ];
 
-  shellHook = ''
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+    with pkgs;
+    [
+      stdenv.cc.cc.lib
+      zlib
+    ]
+  );
+
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    pip install --upgrade pip
+  '';
+
+  postShellHook = ''
+    unset SOURCE_DATE_EPOCH
+
     echo "Osiris Dev Environment Loaded!"
   '';
 }
